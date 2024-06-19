c4bf41c4545bd910f25e3ca0cdac3394e7f5582f
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2024-06-18 22:15:54 -0300

    A estrutura do projeto

---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log('API Key:', process.env.API_KEY);
+console.log('API URL:', process.env.API_URL);
+console.log('Wilhem Wundt Maxime');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv()
+  ],
+  mode: 'development'
+};
